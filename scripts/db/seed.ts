44fbbca4aa2d5b68d30b864df2ba77b825d7bec5
Philipp Runge <p.runge@progani.com>
2025-11-21 22:27:30 +0100

    remove seed endpoint and add cli script instead

--- scripts/db/seed.ts
+++ scripts/db/seed.ts
@@ -1,7 +1,7 @@
 // fetch sets from external API and insert into the database
-import { cardsTable, db, Rarity } from ".";
-import pokemonAPI from "../pokemon-api";
-import { setsTable } from "./index";
+import { env } from "@/env";
+import { cardsTable, db, Rarity, setsTable } from "@/lib/db";
+import pokemonAPI from "@/lib/pokemon-api";
 
 export async function fetchAndStoreSets(withCards = true) {
   console.log("Fetching and storing sets...");
@@ -86,3 +86,47 @@ export async function fetchAndStoreCards(setId: string) {
     console.error("Error fetching or storing cards:", error);
   }
 }
+
+async function run() {
+  const dbHost = env.DATABASE_URL
+    ? env.DATABASE_URL.split("@")[1]?.split("/")[0]?.split(":")[0]
+    : "unknown host";
+  const dbUser = env.DATABASE_URL
+    ? env.DATABASE_URL.split("//")[1]?.split(":")[0]
+    : "unknown user";
+
+  if (dbHost !== "localhost") {
+    const readline = await import("readline");
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const answer = await new Promise<string>((resolve) => {
+      rl.question(
+        `⚠️  WARNING: You are about to seed a non-local database\n${dbUser}@${dbHost}\nDo you want to proceed? (y/N): `,
+        (answer) => {
+          rl.close();
+          resolve(answer);
+        }
+      );
+    });
+
+    if (answer.toLowerCase() !== "y") {
+      console.log("❌  Seeding cancelled.");
+      process.exit(0);
+    }
+  }
+
+  console.log(
+    "Starting to seed database with sets and cards from external API..."
+  );
+
+  await fetchAndStoreSets();
+  console.log("✅  Seeding completed!");
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
