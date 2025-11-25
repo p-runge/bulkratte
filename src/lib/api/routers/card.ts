031b40ba22dead97290b39691ec07eab1b1f39d5
Philipp Runge <p.runge@progani.com>
2025-11-25 18:34:26 +0100

    add get card by id endpoint

--- src/lib/api/routers/card.ts
+++ src/lib/api/routers/card.ts
@@ -2,6 +2,7 @@ import { cardsTable, db } from "@/lib/db";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import z from "zod";
 import { eq } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
 
 export const cardRouter = createTRPCRouter({
   getList: publicProcedure
@@ -20,5 +21,29 @@ export const cardRouter = createTRPCRouter({
         .orderBy(cardsTable.updated_at)
         .limit(input?.setId ? -1 : 100);
       return cards;
+    }),
+
+  getById: publicProcedure
+    .input(
+      z.object({
+        cardId: z.string(),
+      })
+    )
+    .query(async ({ input }) => {
+      const card = await db
+        .select()
+        .from(cardsTable)
+        .where(eq(cardsTable.id, input.cardId))
+        .limit(1)
+        .then((rows) => rows[0] || null);
+
+      if (!card) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: `Card with ID ${input.cardId} not found`,
+        });
+      }
+
+      return card;
     }),
 });
