3e189c9d312ff918793918c326495629d0242ba3
Philipp Runge <p.runge@progani.com>
2026-02-24 01:25:14 +0100

    unplacing cards no longer adds new cards to collection passively

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -51,6 +51,7 @@ function Button({
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      {...(!asChild && { type: "button" })}
       {...props}
     />
   );
