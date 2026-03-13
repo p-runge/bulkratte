f82ca7bb2187bb4cb9c7c3fd9b9fee04bc381bec
Philipp Runge <p.runge@progani.com>
2026-03-13 01:53:34 +0100

    add minIO docker container as local replacement for r2

--- src/env.ts
+++ src/env.ts
@@ -16,10 +16,11 @@ export const env = createEnv({
     AUTH_GOOGLE_CLIENT_ID: z.string(),
     AUTH_GOOGLE_CLIENT_SECRET: z.string(),
     CRON_SECRET: z.string(),
-    R2_ACCOUNT_ID: z.string(),
+    R2_ACCOUNT_ID: z.string().optional(),
     R2_ACCESS_KEY_ID: z.string(),
     R2_SECRET_ACCESS_KEY: z.string(),
     R2_BUCKET_NAME: z.string(),
+    R2_ENDPOINT: z.string().url().optional(),
   },
   client: {
     // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -1,9 +1,13 @@
 import { DeleteObjectsCommand, S3Client } from "@aws-sdk/client-s3";
 import { env } from "@/env";
 
+const endpoint =
+  env.R2_ENDPOINT ?? `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`;
+
 export const r2 = new S3Client({
   region: "auto",
-  endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint,
+  forcePathStyle: !!env.R2_ENDPOINT,
   credentials: {
     accessKeyId: env.R2_ACCESS_KEY_ID,
     secretAccessKey: env.R2_SECRET_ACCESS_KEY,
